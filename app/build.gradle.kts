f025278a1285e25125a46567c627007b4e6fcc0e
ymirotvorenie <karoline.navarra@gmail.com>
2024-07-28 12:02:53 +0700

    add user model

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,10 +25,17 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	//H2 database
+	implementation("com.h2database:h2:2.2.224")
+	implementation("com.zaxxer:HikariCP:5.1.0")
 }
 
 tasks.withType<Test> {
