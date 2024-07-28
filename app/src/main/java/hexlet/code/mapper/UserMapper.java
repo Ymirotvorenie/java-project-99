ba2be610897bb4949ba4c517bbb5c8e234980a1b
ymirotvorenie <karoline.navarra@gmail.com>
2024-07-28 20:38:23 +0700

    add user password encrypt

--- app/src/main/java/hexlet/code/mapper/UserMapper.java
+++ app/src/main/java/hexlet/code/mapper/UserMapper.java
@@ -4,11 +4,9 @@ import hexlet.code.dto.UserCreateDTO;
 import hexlet.code.dto.UserDTO;
 import hexlet.code.dto.UserUpdateDTO;
 import hexlet.code.model.User;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingConstants;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
-import org.mapstruct.ReportingPolicy;
+import org.mapstruct.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Mapper(
         uses = { JsonNullableMapper.class },
@@ -17,7 +15,16 @@ import org.mapstruct.ReportingPolicy;
         unmappedTargetPolicy = ReportingPolicy.IGNORE
 )
 public abstract class UserMapper {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public abstract User map(UserCreateDTO dto);
     public abstract UserDTO map(User model);
     public abstract void update(UserUpdateDTO dto, @MappingTarget User model);
+
+    @BeforeMapping
+    public void encryptPassword(UserCreateDTO data) {
+        var password = data.getPassword();
+        data.setPassword(passwordEncoder.encode(password));
+    }
 }
